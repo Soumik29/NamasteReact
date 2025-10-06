95b3f4723d1d7bbd3b985e10fe322a9d121c2ca6
Soumik Mitra <soumikdeb2@gmail.com>
2025-10-06 18:08:10 -0400

    The API is messed up

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -1,19 +1,23 @@
 import { CDN_URL } from "../utils/constants";
 const MenuItems = ({ menuCategory }) => {
-//   console.log(menuCategory);
-//   console.log("This message is just after menuCategory")
+  //   console.log(menuCategory);
+  //   console.log("This message is just after menuCategory")
   const maxLength = 100;
   console.log(menuCategory);
-  if(!menuCategory) return null;
+  if (!menuCategory) return null;
   return (
     <div>
       {menuCategory.map((menu) => {
         const s = menu.card?.info;
         return (
-          <div>
-            <h2>{s.name}</h2>
+          <div key={s.id}>
+            <div className="flex flex-col">
+              <h2 className="font-medium">{s.name}</h2>
+              <span>₹{s.price ? s.price / 100 : s.defaultPrice / 100}</span>
+              <span>⭐{s.ratings.aggregatedRating.rating}({s.ratings.aggregatedRating.ratingCountV2})</span>
+            </div>
           </div>
-        )
+        );
       })}
     </div>
   );
--- src/components/MenuTitles.js
+++ src/components/MenuTitles.js
@@ -9,24 +9,36 @@ const MenuTitles = ({ categories }) => {
             const categoryInfo = category.card.card;
 
             if (categoryInfo.categories) {
+              console.log("Is this even getting printed");
               return (
-                <div key={index}>
+                <div
+                  key={index}
+                  className="w-full p-4 mb-4 border rounded-xl shadow-md"
+                >
                   <h1>{categoryInfo.title}</h1>
                   {categoryInfo.categories.map((subCategories, subIndex) => (
-                    <div key={subIndex} className="ml-2 mt-4">
-                      <h2 className="text-lg font-semibold">{subCategories.title}</h2>  
+                    <div key={subIndex} className="ml-2 mt-4 w-full">
+                      {subCategories.title !== categoryInfo.title && (
+                        <div className="flex justify-between">
+                          <h2 className="text-lg font-semibold">
+                            {subCategories.title}
+                          </h2>
+                          <span>🔽</span>
+                        </div>
+                      )}
                       <MenuItems menuCategory={subCategories.itemCards} />
                     </div>
                   ))}
                 </div>
               );
             }
+            console.log("This is the middle statement");
             return (
               <div key={index} className="w-full p-4 mb-4 border-4">
                 <h1 className="text-xl font-bold">{categoryInfo.title}</h1>
-                <MenuItems menuCategory={categoryInfo.itemCards}/>
+                <MenuItems menuCategory={categoryInfo.itemCards} />
               </div>
-            )
+            );
           })}
         </div>
       </div>
